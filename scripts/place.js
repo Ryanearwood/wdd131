b6e13e3da0718becb5797455fc927d674b869b0d
Ryan Earwood <ear24003@byui.edu>
2024-05-10 17:44:23 -0400

    2

--- scripts/place.js
+++ scripts/place.js
@@ -2,11 +2,26 @@ window.onload = function() {
     // Display the windchill factor
     var temperature = 20; // replace with your static temperature value
     var windSpeed = 5; // replace with your static wind speed value
-    var windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-    document.getElementById('weather-box').innerHTML += '<p>Wind Chill: ' + windChill.toFixed(2) + '°F</p>';
+    var windChill = calculateWindChill(temperature, windSpeed);
+    var weatherBox = document.getElementById('weather-box');
+    if (windChill !== "N/A") {
+        weatherBox.innerHTML += '<p>Wind Chill: ' + windChill.toFixed(2) + '°F</p>';
+    } else {
+        weatherBox.innerHTML += '<p>Wind Chill: N/A</p>';
+    }
 
     // Display the current year and the date the document was last modified
     var currentYear = new Date().getFullYear();
     document.getElementById('footer').innerHTML = document.getElementById('footer').innerHTML.replace('2024', currentYear);
     document.getElementById('lastModified').textContent = document.lastModified;
+}
+
+function calculateWindChill(temperature, windSpeed) {
+    var windChill;
+    if (temperature <= 10 && windSpeed > 3) { // Check if conditions are met for wind chill calculation
+        windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+        return windChill;
+    } else {
+        return "N/A";
+    }
 }
